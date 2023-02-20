39cdc6ba928672ac83c6c087f4904043919b70cf
= <=>
2023-02-20 17:06:38 +0530

    init

---
+++ server.mjs
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+import express from 'express';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const port = 4000;
+
+const staticFilePath = path.join(__dirname, '../dist');
+
+const application = express();
+application.disable("x-powered-by");
+application.use(express.static(staticFilePath));
+
+application.get('/*', (req, res) => {
+  res.sendFile(`${staticFilePath}/index.html`);
+});
+
+application.listen(port);
